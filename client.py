dac017694d5f0605178ea243de5a1ef17c4eb001
Matthew Suozzo <msuozzo@google.com>
2017-02-25 16:31:35 -0500

    Add basic argparse to client and server

--- client.py
+++ client.py
@@ -1,16 +1,35 @@
+import argparse
 import pprint
-import ssl
 import socket
+import ssl
 
-ctx = ssl.SSLContext(ssl.PROTOCOL_TLS)
-ctx.verify_mode = ssl.CERT_REQUIRED
-ctx.check_hostname = False
-ctx.load_cert_chain('client_cert.pem', keyfile='client_key.pem')
-ctx.load_verify_locations('server_cert.pem')
-conn = ctx.wrap_socket(socket.socket(socket.AF_INET))
 
-conn.connect(('localhost', 8888))
+def connect(hostname, port):
+  ctx = ssl.SSLContext(ssl.PROTOCOL_TLS)
+  ctx.verify_mode = ssl.CERT_REQUIRED
+  ctx.check_hostname = False
+  ctx.load_cert_chain('client_cert.pem', keyfile='client_key.pem')
+  ctx.load_verify_locations('server_cert.pem')
+  conn = ctx.wrap_socket(socket.socket(socket.AF_INET))
 
-pprint.pprint(conn.getpeercert())
+  conn.connect((hostname, port))
 
-conn.close()
+  pprint.pprint(conn.getpeercert())
+
+  conn.close()
+
+
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser()
+  parser.add_argument(
+      'serv_addr',
+      help='The address at which the server is running')
+  parser.add_argument(
+      'serv_port',
+      help='The port on which the server is running',
+      type=int)
+
+  # Parse the commandline arguments.
+  args = parser.parse_args()
+
+  connect(args.serv_addr, args.serv_port)
--- server.py
+++ server.py
@@ -1,18 +1,33 @@
+import argparse
 import pprint
-import ssl
 import socket
+import ssl
 
-ctx = ssl.SSLContext(ssl.PROTOCOL_TLS)
-ctx.verify_mode = ssl.CERT_REQUIRED
-ctx.check_hostname = False
-ctx.load_cert_chain('server_cert.pem', keyfile='server_key.pem')
-ctx.load_verify_locations('client_cert.pem')
-conn = ctx.wrap_socket(socket.socket(socket.AF_INET), server_side=True)
 
-conn.bind(('localhost', 8888))
-conn.listen()
-new_conn, addr = conn.accept()
+def connect(port):
+  ctx = ssl.SSLContext(ssl.PROTOCOL_TLS)
+  ctx.verify_mode = ssl.CERT_REQUIRED
+  ctx.load_cert_chain('server_cert.pem', keyfile='server_key.pem')
+  ctx.load_verify_locations('client_cert.pem')
+  conn = ctx.wrap_socket(socket.socket(socket.AF_INET), server_side=True)
+  
+  conn.bind(('localhost', port))
+  conn.listen()
+  new_conn, addr = conn.accept()
+  
+  pprint.pprint(new_conn.getpeercert())
+  
+  new_conn.close()
 
-pprint.pprint(new_conn.getpeercert())
 
-new_conn.close()
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser()
+  parser.add_argument(
+      'port',
+      help='The port on which the server should run',
+      type=int)
+
+  # Parse the commandline arguments.
+  args = parser.parse_args()
+
+  connect(args.port)
