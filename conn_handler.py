55afbb3256011e9758b2419d4e15bc823584d864
Matthew Suozzo <msuozzo@google.com>
2017-03-07 13:56:18 -0500

    Refactor connection handler

--- conn_handler.py
+++ conn_handler.py
@@ -1,24 +1,23 @@
-import socket
 
-def send_data(s,filename):
+
+def send_data(sock, filename):
+    """Send data from a file to the socket."""
     with open(filename, 'rb') as r:
        data = r.read()
        # check data length in bytes and send it to client
        data_length = len(data)
-       s.sendall(data_length.to_bytes(4, 'big'))
-       s.sendall(data)
-    #s.shutdown(socket.SHUT_RDWR)
-    #s.close()
+       sock.sendall(data_length.to_bytes(4, 'big'))
+       sock.sendall(data)
 
-def recv_data(clientsocket,filename):
-    # check expected message length
-    remaining = int.from_bytes(clientsocket.recv(4), 'big')
-    d = open(filename, "wb")
-    while remaining:
-        # until there are bytes left...
-        # fetch remaining bytes or 1024 (whatever smaller)
-        rbuf = clientsocket.recv(min(remaining, 1024))
-        remaining -= len(rbuf)
-        # write to file
-        d.write(rbuf)
-    d.close()
+
+def recv_data(sock, filename):
+    """Receive data from the socket into a file."""
+    # Get the size of the expected data.
+    remaining = int.from_bytes(sock.recv(4), 'big')
+    with open(filename, 'wb') as f:
+        while remaining:
+            # While we expect to receive more data, gather that much data (or
+            # a full chunk, whichever is smaller).
+            rbuf = sock.recv(min(remaining, 1024))
+            remaining -= len(rbuf)
+            f.write(rbuf)
